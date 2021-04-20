92d5da69ec4ee284c1d8c3fe0db8f9a697d42055
bjgman12 <bjgman38@gmail.com>
2021-04-20 15:56:25 -0700

    features for react lll complete

--- components/form.js
+++ components/form.js
@@ -27,26 +27,32 @@ export default function CookieStandForm({ onCreate }) {
         setValues({ ...values, [name]: value });
     }
     return (
-        <form onSubmit={submitHandler}>
-            <div>
-                <label htmlFor="location">Add Location</label>
-                <input type="text" name="location" id="location" value={values.location} onChange={inputChangeHandler} placeholder="Cookie Stand Location" />
+        <form onSubmit={submitHandler} className='w-4/5 bg-green-200 mx-auto mt-6 border-2 border-green-800 rounded-lg grid grid-cols-3 grid-rows-2 pl-10 pt-8 pl-6 pb-3 gap-8'>
+            <div className='col-span-2 text-center w-full  h-1/2'>
+                <label className='font-bold 'htmlFor="location">Add Location</label>
+                <input className='w-full h-10 border-2 border-gray-100'type="text" name="location" id="location" value={values.location} onChange={inputChangeHandler} placeholder="  Cookie Stand Location" />
             </div>
 
-            <button type="submit">Create Stand</button>
+            <button type="submit" className='bg-green-600 mx-4 h-3/4 rounded-lg font-medium'>Create Stand</button>
 
 
             <FormInputSection>
-                <label htmlFor="min">Minimum Customers per Hour</label>
-                <input type="number" name="min" id="min" value={values.min} onChange={inputChangeHandler} />
+                <div className='text-center mb-4'>
+                <label  className='font-bold' htmlFor="min">Minimum Customers per Hour</label>
+                <input className='p-2  border-2 border-gray-200 w-4/5'type="number" name="min" id="min" value={values.min} onChange={inputChangeHandler} />
+                </div>
             </FormInputSection>
             <FormInputSection>
-                <label htmlFor="max">Maximum Customers per Hour</label>
-                <input type="number" name="max" id="max" value={values.max} onChange={inputChangeHandler} />
+                <div className='text-center'>
+                <label className='font-bold' htmlFor="max">Maximum Customers per Hour</label>
+                <input  className='p-2  border-2 border-gray-200 w-4/5'type="number" name="max" id="max" value={values.max} onChange={inputChangeHandler} />
+                </div>
             </FormInputSection>
             <FormInputSection>
-                <label htmlFor="avg">Average Cookies per Sale</label>
-                <input type="number" name="avg" id="avg" value={values.avg} onChange={inputChangeHandler} />
+                <div className='text-center'>
+                <label className='font-bold' htmlFor="avg">Average Cookies per Sale</label>
+                <input  className='p-2  border-2 border-gray-200 w-4/5' type="number" name="avg" id="avg" value={values.avg} onChange={inputChangeHandler} />
+                </div>
             </FormInputSection>
         </form>
     );
